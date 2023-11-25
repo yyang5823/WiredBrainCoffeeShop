6c16ecc12a982bb6217916b24c4a46be8738e5f2
yyang5823 <yyang5823@gmail.com>
2023-11-25 14:40:11 -0500

    Add CoffeeShopDataProvider class and app project file

---
+++ WiredBrainCoffeeShop/Data/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace WiredBrainCoffeeShop.Data
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadShops()
+        {
+            return new CoffeeShop[]
+            {
+                new CoffeeShop("Frankfaut", 45),
+                new CoffeeShop("Zuich", 39),
+                new CoffeeShop("Rome", 25)
+            }
+        }
+    }
+}
