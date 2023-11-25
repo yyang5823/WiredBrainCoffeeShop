0034a68d21a4df4d69a802093af497841dce2065
yyang5823 <yyang5823@gmail.com>
2023-11-24 20:15:10 -0500

    Add project files.

---
+++ WiredBrainCoffeeShop/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("------------------------------------------");
+Console.WriteLine("   Wired Brain Coffee Shop - Info Tools   ");
+Console.WriteLine("------------------------------------------");
+Console.ReadLine();
+
